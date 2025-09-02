06f8cfc1d0deb276faabea3c58721a6ce8cd23af
NadiaHirwa <irahirnad22@gmail.com>
2025-09-02 21:09:48 +0300

    worked on my skills part

--- src/components/SkillsSection.jsx
+++ src/components/SkillsSection.jsx
@@ -2,29 +2,39 @@ import { useState } from "react";
 import { cn } from "@/lib/utils";
 
 const skills = [
+  // Data Analysis
+  { name: "Python", level: 80, category: "Data Analysis" },
+  { name: "SQL", level: 80, category: "Data Analysis" },
+  { name: "Dax", level: 75, category: "Data Analysis" },
+  { name: "Looker Studio", level: 80, category: "Data Analysis" },
+  { name: "Plotly/Dash", level: 80, category: "Data Analysis" },
+  { name: "Machine Learning", level: 55, category: "Data Analysis" },
+  { name: "Quarto", level: 75, category: "Data Analysis" },
+
   // Frontend
   { name: "HTML/CSS", level: 95, category: "frontend" },
-  { name: "JavaScript", level: 90, category: "frontend" },
-  { name: "React", level: 90, category: "frontend" },
-  { name: "TypeScript", level: 85, category: "frontend" },
-  { name: "Tailwind CSS", level: 90, category: "frontend" },
-  { name: "Next.js", level: 80, category: "frontend" },
+  { name: "JavaScript", level: 70, category: "frontend" },
+  { name: "React", level: 60, category: "frontend" },
+  { name: "Tailwind CSS", level: 30, category: "frontend" },
+  { name: "Flutter", level: 70, category: "frontend" },
 
   // Backend
-  { name: "Node.js", level: 80, category: "backend" },
-  { name: "Express", level: 75, category: "backend" },
-  { name: "MongoDB", level: 70, category: "backend" },
-  { name: "PostgreSQL", level: 65, category: "backend" },
-  { name: "GraphQL", level: 60, category: "backend" },
+  { name: "Node.js", level: 70, category: "backend" },
+  { name: "Express", level: 50, category: "backend" },
+  { name: "MongoDB", level: 60, category: "backend" },
+  { name: "PostgreSQL", level: 60, category: "backend" },
+  { name: "SQLite", level: 80, category: "backend" },
 
   // Tools
-  { name: "Git/GitHub", level: 90, category: "tools" },
-  { name: "Docker", level: 70, category: "tools" },
+  { name: "Git/GitHub", level: 80, category: "tools" },
+  { name: "Excel", level: 90, category: "tools" },
   { name: "Figma", level: 85, category: "tools" },
   { name: "VS Code", level: 95, category: "tools" },
+  { name: "Power BI", level: 80, category: "tools" },
+  { name: "Jupyter Notebook", level: 80, category: "tools" },
 ];
 
-const categories = ["all", "frontend", "backend", "tools"];
+const categories = ["all", "Data Analysis", "frontend", "backend", "tools"];
 
 export const SkillsSection = () => {
   const [activeCategory, setActiveCategory] = useState("all");
